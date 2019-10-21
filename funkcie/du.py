6fdf1c31a6aca90be50aa801ad02107812c7be0f
Miklosovic <56343557+Miklosovic@users.noreply.github.com>
2019-10-21 18:14:43 +0200

    Add files via upload

---
+++ funkcie/du.py
@@ -0,0 +1,24 @@
+import tkinter
+import random
+canvas=tkinter.Canvas(height=600,width=800,bg="white")
+canvas.pack()
+
+def strom (x,y,r):
+    canvas.create_rectangle(x-10,y+10,x+10,y+110, fill="brown", outline="brown")
+    canvas.create_oval (x+r,y+r,x-r,y-r, fill="green", outline="green")
+
+
+def trava (x,y,s,v):
+    for i in range (3,10):
+        canvas.create_line(x,y,s,v, fill="green", width=random.randint(1,2))       
+        v-=random.randrange(3,7)
+        s+=random.randrange(3,10)
+for j in range (5,15):
+    x=random.randrange(1,800)
+    y=random.randrange(1,600)
+    trava (x,y,x+random.randrange(-25,25),y-random.randrange(0,5))
+for k in range (5,15):
+    x=random.randrange(1,800)
+    y=random.randrange(1,600)
+    strom (x,y,random.randint(10,50))
+
