df4d95abb55160c71d3816ab7c764caab90b2db0
Miklosovic <56343557+Miklosovic@users.noreply.github.com>
2019-10-23 12:31:18 +0200

    Add files via upload

---
+++ funkcie/vlaciky.py
@@ -0,0 +1,18 @@
+import tkinter
+import random
+X_MAX, Y_MAX = 800, 600
+canvas = tkinter.Canvas(width=X_MAX, height=Y_MAX)
+canvas.pack()
+
+def farba():
+    r=random.randrage(256)
+    g=random.randrange(256)
+    b=random.randrange(256)
+    return f"#{r:02x}{g:02x}{b:02x}
+
+def vlak():
+    for i in range(2,5):
+        canvas.create_rectangle(x-r-r/5,y-9*d/10,x+r+r/5,y-8*d/10, fill="black")
+        canvas.create_rectangle(x-r,y-d,x+r,y+d, fill=farba)
+        canvas.create_oval()
+        
