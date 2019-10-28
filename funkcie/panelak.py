fa3eb051a84d7baf159d3be7ecfffa95d3cd44b6
Miklosovic <56343557+Miklosovic@users.noreply.github.com>
2019-10-28 17:14:50 +0100

    Update panelak.py

--- funkcie/panelak.py
+++ funkcie/panelak.py
@@ -6,21 +6,16 @@ canvas.pack()
 def panelak(x,y,v,s):
     
     canvas.create_rectangle(x,y,x+20*s-10,y+20*v-10)
-    p=y
        
-    for i in range (1,s):
-        
-        for j in range(1,v):
+    for _ in range (1,s):
+        p=y 
+        for _ in range(1,v):
             canvas.create_rectangle(x+10,y+10,x+20,y+20)
-            y=y+20
+            y+=20
         x+=20
-        y=p
-           
-            
-            
-            
+        
 def mesto():
-    for i in range(1,10):
+    for _ in range(1,10):
         x=random.randrange(10,770)
         y=random.randrange(10,770)
         v=random.randrange(3,10)
