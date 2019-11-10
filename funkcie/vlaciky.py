24806aa73fb825ef59a3a85fb290c6de53edc2ec
Miklosovic <56343557+Miklosovic@users.noreply.github.com>
2019-11-10 10:07:35 +0100

    Update vlaciky.py

--- funkcie/vlaciky.py
+++ funkcie/vlaciky.py
@@ -10,9 +10,20 @@ def farba():
     b=random.randrange(256)
     return f"#{r:02x}{g:02x}{b:02x}"
 
-def vlak():
-    for i in range(2,5):
-        canvas.create_rectangle(x-r-r/5,y-9*d/10,x+r+r/5,y-8*d/10, fill="black")
-        canvas.create_rectangle(x-r,y-d,x+r,y+d, fill=farba)
-        canvas.create_oval()
-        
+def vlak(x,y,r,d,f):
+    canvas.create_rectangle(x-r-r/5,y+d/5,x+r+r/5,y+2*d/5, fill="black")
+    canvas.create_rectangle(x-r,y-d,x+r,y+d, fill=f)
+    canvas.create_oval(x-7*r/8,y+3*d/5,x-r/8,y+7*d/5, fill="black")
+    canvas.create_oval(x+7*r/8,y+3*d/5,x+r/8,y+7*d/5, fill="black")
+
+x=200
+y=100
+r=80
+d=50
+for _ in range(3):
+    a=random.randint(1,3)
+    for _ in range(a):
+        vlak(x,y,r,d,farba())
+        x+=2*r+2*r/5
+    x=200
+    y+=200       
