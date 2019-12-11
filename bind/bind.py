4b7babac92a7ba1458aedd30c5dd9554ed73909a
Miklosovic <56343557+Miklosovic@users.noreply.github.com>
2019-12-11 12:21:16 +0100

    Add files via upload

--- bind/bind.py
+++ bind/bind.py
@@ -6,10 +6,13 @@ c.pack()
 
 def pohyb(event):
     r = 8
-    x,y = event.x, event.y
-    c.create_oval (x-r,y-r,x+r,y+r, fill = "blue")
+    x, y = event.x, event.y
     a = x_max - x
+    b = y_max - y
+    c.create_oval (x-r,y-r,x+r,y+r, fill = "blue")
     c.create_oval (a-r,y-r,a+r,y+r, fill = "orange")
+    c.create_oval (x-r,b-r,x+r,b+r, fill = "yellow")
+    c.create_oval (a-r,b-r,a+r,b+r, fill = "green")
 
 c.bind("<Motion>", pohyb)
     
