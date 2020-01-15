d81fd3de72aeb39b188a8185b3bd85e719bd6abc
Miklosovic <56343557+Miklosovic@users.noreply.github.com>
2020-01-15 11:56:09 +0100

    Add files via upload

---
+++ EDUPAGE.py
@@ -0,0 +1,37 @@
+import math
+pi = math.pi
+
+##1
+
+##x = int(input("Prve cislo: "))
+##y = int(input("Druhe cislo: "))
+##vysledok = int(x/y)
+##zvysok = x - int(vysledok) * y
+##print (vysledok, "zvysok je ", zvysok)
+
+##2
+
+##x = int(input("Strana stvorca: "))
+##obvod = x*4
+##obsah = x**2
+##print ("obvod je ", obvod, "obsah je ", obsah)
+
+##3
+
+##a = int(input("prva strana: "))
+##b = int(input("druha strana: "))
+##c = int(input("tretia strana: "))
+##povrch = 2*(a*b + b*c + a*c)
+##objem = a*b*c
+##print ("povrch je ", povrch, "objem je ", objem)
+
+##4
+
+##r = float(input("polomer: "))
+##obvod = 2*pi*r
+##obsah = pi*(r**2)
+##objem = (4/3)*pi*(r**3)
+##print("obvod je ", obvod, "obsah je ", obsah, "objem je ", objem)
+
+
+
