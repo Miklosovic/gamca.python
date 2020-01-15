7a11dbfcafab79dcb68b140960cfa8b14325f728
Miklosovic <56343557+Miklosovic@users.noreply.github.com>
2020-01-15 11:55:21 +0100

    Delete edu3.py

--- EDUPAGE/edu3.py
+++
@@ -1,6 +0,0 @@
-a = int(input("prva strana: "))
-b = int(input("druha strana: "))
-c = int(input("tretia strana: "))
-povrch = 2*(a*b + b*c + a*c)
-objem = a*b*c
-print ("povrch je ", povrch, "objem je ", objem)
