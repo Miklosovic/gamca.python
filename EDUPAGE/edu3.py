e49853d3b20cd7b3462ec24d1c606ff34134cc94
Miklosovic <56343557+Miklosovic@users.noreply.github.com>
2020-01-15 11:44:12 +0100

    Add files via upload

---
+++ EDUPAGE/edu3.py
@@ -0,0 +1,6 @@
+a = int(input("prva strana: "))
+b = int(input("druha strana: "))
+c = int(input("tretia strana: "))
+povrch = 2*(a*b + b*c + a*c)
+objem = a*b*c
+print ("povrch je ", povrch, "objem je ", objem)
